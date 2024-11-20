031778e017ba231dbfa6ba797d7b72b9e963fe2c
Tharinivarsha <chitraswamy11@gmail.com>
2024-11-20 16:48:00 +0530

    SQL Create table (Project)

---
+++ activity.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Customer(
+    CustomerID INT PRIMARY KEY,
+    CustomerName TEXT,
+    LastName TEXT,
+    Country TEXT,
+    Age TEXT,
+    Phone TEXT
+);
+INSERT INTO Customer (CustomerID, CustomerName, LastName, Country, Age, Phone)
+VALUES (1, 'Shubham', 'Thakur', 'India','23','xxxxxxxxxx'),
+       (2, 'Aman ', 'Chopra', 'Australia','21','xxxxxxxxxx'),
+       (3, 'Naveen', 'Tulasi', 'Sri lanka','24','xxxxxxxxxx'),
+       (4, 'Aditya', 'Arpan', 'Austria','21','xxxxxxxxxx'),
+       (5, 'Nishant. Salchichas S.A.', 'Jain', 'Spain','22','xxxxxxxxxx');
+SELECT * FROM Customer
